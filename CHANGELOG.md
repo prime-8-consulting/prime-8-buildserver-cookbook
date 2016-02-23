d27770a9cc748d2184aa8e0a6ea59ba842217a76
munjeli <elemunjeli@gmail.com>
2016-02-23 11:46:25 -0800

    generated basic files

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Prime 8 Buildserver Cookbook CHANGELOG
+======================================
+
+v0.0.1 (2016-02-23)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# Encoding: UTF-8
+#
+
+name             'prime-8-buildserver'
+maintainer       'prime 8 consulting'
+maintainer_email 'ellenm@prime8consulting.com'
+license          'apache2'
+description      'Installs/Configures prime-8-buildserver'
+long_description 'Installs/Configures prime-8-buildserver'
+version          '0.0.1'
