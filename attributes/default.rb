1570ccd96086d5a886403935bc4f5c07e6d3c8db
munjeli <elemunjeli@gmail.com>
2016-03-04 11:03:50 -0800

    improved recipe for buildAgents

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default["teamcity_server"]["version"] = "9.1.6"
+default["teamcity_server"]["build_agent"]["server"] = "localhost:8111"
+default["teamcity_server"]["build_agent"]["name"] = "cloud8" 
