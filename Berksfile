a3510ee71bf0620b2c3d5535c370fb8a7418e74c
munjeli <elemunjeli@gmail.com>
2016-05-24 16:07:29 -0700

    removed mongo

--- Berksfile
+++ Berksfile
@@ -3,10 +3,8 @@
 source 'https://supermarket.chef.io'
 
 metadata
-cookbook 'teamcity_server'
 cookbook 'sbp_packer'
 cookbook 'jenkins'
 cookbook 'nodejs'
-cookbook 'mongodb3'
 cookbook 'terraform'
 cookbook 'consul'
--- metadata.rb
+++ metadata.rb
@@ -9,11 +9,8 @@ description      'Installs/Configures prime-8-buildserver'
 long_description 'Installs/Configures prime-8-buildserver'
 version          '0.0.2'
 
-depends 'teamcity_server'
 depends 'jenkins'
 depends 'sbp_packer'
-depends 'mongodb3'
-depends 'nodejs'
 depends 'terraform'
 depends 'consul'
-
+depends 'nodejs'
