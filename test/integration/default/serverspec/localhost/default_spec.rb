a4335b71ace4354167c049182ad862f43fa29b3e
munjeli <elemunjeli@gmail.com>
2016-05-24 15:35:31 -0700

    spec tests

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -3,10 +3,27 @@
 require_relative '../spec_helper'
 
 describe 'prime-8-buildserver::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
 
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+  describe '.cloud8 filesystem' do 
+    describe file('/var/lib/jenkins/.cloud8') do 
+      it { should be_directory }
+      it { should be_owned_by 'jenkins' }
+      it { should be_mode '0755' }
+    end
+    describe file('/var/lib/jenkins/.cloud8/creds') do 
+      it { should be_directory }
+      it { should be_owned_by 'jenkins' }
+      it { should be_mode '0755' }
+    end
+    describe file('/var/lib/jenkins/.cloud8/repos') do 
+      it { should be_directory }
+      it { should be_owned_by 'jenkins' }
+      it { should be_mode '0755' }
+    end
+    describe file('/var/lib/jenkins/.cloud8') do 
+      it { should be_directory }
+      it { should be_owned_by 'jenkins' }
+      it { should be_mode '0755' }
+    end
+  end 
 end
