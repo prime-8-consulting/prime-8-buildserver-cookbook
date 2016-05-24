037aa2445d2aec1dd2471ab5fc8291372333ce19
munjeli <elemunjeli@gmail.com>
2016-05-24 15:46:27 -0700

    removed teamcity details from attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,3 @@
-default["teamcity_server"]["version"] = "9.1.6"
-default["teamcity_server"]["build_agent"]["server"] = "localhost:8111"
-default["teamcity_server"]["build_agent"]["name"] = "cloud8" 
 default['packer']['version'] = '0.10.0'
 default['packer']['url_base'] = "https://releases.hashicorp.com/packer/#{node['packer']['version']}" 
 default['packer']['raw_checksums'] = <<-EOF
